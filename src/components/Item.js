50af2e2a9dd4704a43f8b7332fbaefe16744e6b2
dmoneyballer <drew.gilliesbc@gmail.com>
2019-03-02 03:00:38 -0700

    for use on laptop

---
+++ src/components/Item.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+class Item extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            name: this.props.name,
+            currentPrice: 0,
+            historicHigh: 0,
+            historicLow: 0
+        }
+    }
+    
+}
+
+export default Item;
